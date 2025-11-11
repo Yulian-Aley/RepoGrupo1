97e5db7b0c0f504989328f81fc39e14049cefc3f
Yulian-Aley <yulianaley@unicomfacauca.edu.co>
2025-11-11 08:40:55 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 10000;
-
-app.get("/", (req, res) => {
-  res.send("¡Hola Render! 🚀 Este es mi primer despliegue.");
-});
-
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
